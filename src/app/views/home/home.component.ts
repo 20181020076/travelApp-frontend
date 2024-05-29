c89b1180203e1bf05de55f640ca7788fa49abbf7
Diego <diegoarojas@hotmail.es>
2024-05-29 13:24:44 -0500

    fix rendering data from api

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -13,11 +13,11 @@ import { CountriesService } from '../../services/countries.service';
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit{
-  countries: string[] = [];
-  cities: string[] = ['Toronto', 'Roma', 'Bogota', 'Paris'];
-  formState: number = 2;
-  selectedCountry: string = this.countries[0];
-  selectedCity: string = this.cities[0];
+  countries: any[] = [];
+  cities: any[] = [];
+  formState: number = 0;
+  selectedCountry: any = 1;
+  selectedCity: any = this.cities[0];
   selectedCash: number = 0;
   currencyConverted: number = 0;
   weather:number = 0;
@@ -28,10 +28,20 @@ export class HomeComponent implements OnInit{
   ) {}
   ngOnInit(): void {
       this.CountriesService.getCountries().subscribe({
-        next: (x) => {
-          x.map((c:any)=>{
-            return console.log(c)
-          })
+        next: async(x) => {
+          await x.map((country:any,index:number)=>{
+
+            for(let city of country.cities){
+              
+
+              this.cities.push(city)
+           
+            }
+            
+            return this.countries.push(country)
+          });
+          
+
           
           // this.countries=x.
         },
@@ -58,35 +68,35 @@ export class HomeComponent implements OnInit{
     this.formState++;
 
     // metodo que extrae las coordenadas de una ciudad y luego consluta el clima de esas coordenadas
-    this.WeatherService.getLocation({ city: 'hd', country: '' }).subscribe({
-      next: (v) => {
-        const lat = v[0].lat;
-        const lon = v[0].lon;
+    // this.WeatherService.getLocation({ city: 'hd', country: '' }).subscribe({
+    //   next: (v) => {
+    //     const lat = v[0].lat;
+    //     const lon = v[0].lon;
 
-         this.WeatherService.getWeather({lat,lon}).subscribe({
-          next: (x) => {
-            console.log(x.main.temp)
-            this.weather=x.main.temp
-          },
-          error: (er) => console.error(er),
-          complete: () => console.info('complete'),
-        });
+    //      this.WeatherService.getWeather({lat,lon}).subscribe({
+    //       next: (x) => {
+    //         console.log(x.main.temp)
+    //         this.weather=x.main.temp
+    //       },
+    //       error: (er) => console.error(er),
+    //       complete: () => console.info('complete'),
+    //     });
 
         
-      },
-      error: (e) => console.error(e),
-      complete: () => console.info('complete'),
-    });
+    //   },
+    //   error: (e) => console.error(e),
+    //   complete: () => console.info('complete'),
+    // });
 
     // metodo que conviete la moneda de cop a moneda local (descomentar)
 
-    this.ExchangeService.convert().subscribe({
-      next: (v) => {
-        this.currencyConverted = v.conversion_rate*this.selectedCash;
-      },
-      error: (e) => console.error(e),
-      complete: () => console.info('complete'),
-    });
+    // this.ExchangeService.convert().subscribe({
+    //   next: (v) => {
+    //     this.currencyConverted = v.conversion_rate*this.selectedCash;
+    //   },
+    //   error: (e) => console.error(e),
+    //   complete: () => console.info('complete'),
+    // });
 
     const objectData = {
       country: this.selectedCountry,
@@ -102,7 +112,23 @@ export class HomeComponent implements OnInit{
   }
   setSelectedContry(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
+    console.log(selectElement.value)
+    for(let city of this.cities){
+      if(city.id === selectElement.value){
+        this.selectedCity=city
+        console.log(city,"hola")
+
+      }
+    }
+
+    console.log(selectElement.value)
     this.selectedCountry = selectElement.value;
+
+
+  }
+  getCountryNamebyId(id:Number){
+    const validation = this.countries.find(country => country.id === id)
+    return validation?.name
   }
   setSelectedCity(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
